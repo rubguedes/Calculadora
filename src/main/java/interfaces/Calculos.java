8050ed390087823feee1fd26fb58cb295105caf2
Ruben Guedes <rubemvg@outlook.com>
2022-02-20 16:19:59 -0300

    Projeto iniciado

---
+++ src/main/java/interfaces/Calculos.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package interfaces;
+
+/**
+ *
+ * @author rubem
+ */
+public interface Calculos {
+    double soma();
+    double subtracao();
+    double divisao();
+    double multiplicacao();
+}
